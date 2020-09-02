82f2203222df769cc8a00e9e7c761d29fc155bfb
chadlof <chad@chads-MacBook-Pro.local>
2020-09-02 09:01:36 -0500

    Need to fix refetch

--- pages/index.js
+++ pages/index.js
@@ -10,15 +10,21 @@ display:flex;
 flex-flow:column;
 justify-content: center;
 align-items: center;
+width:80%;
+margin:auto;
 `
 const HeaderWrapper = styled.div`
 display:flex;
 justify-content: space-between;
-width: 60%;
+width: 100%;
 align-items: center;
 margin: 100px auto 60px;
 padding-bottom:20px;
 border-bottom: solid 1px #cccbcb;
+`
+const JokeWrapper = styled.div`
+display: inline-block;
+width: 60%;
 `
 const GetJokeButton = styledUI(Button)({
   background: '#92d292',
@@ -29,7 +35,7 @@ const GetJokeButton = styledUI(Button)({
   padding: '0 30px',
   fontSize: 10,
 })
-const HideJokeButton = styledUI(Button)({
+const PunchButton = styledUI(Button)({
   background: '#6facd2',
   border: 0,
   borderRadius: 20,
@@ -37,8 +43,23 @@ const HideJokeButton = styledUI(Button)({
   height: 48,
   padding: '0 30px',
   fontSize: 10,
-  margin: 60,
+  margin: 40,
+  minWidth: 140,
 })
+const Setup = styled.p`
+  display:flex;
+  justify-content:flex-start;
+`
+const PunchButtonWrapper = styled.div`
+display:flex;
+justify-content: center;
+align-items: center;
+width:80%;
+`
+const PunchLine = styled.p`
+  display:flex;
+  justify-content:flex-end;
+`
 
 
 const APILinkButton = styledUI(Button)({
@@ -47,30 +68,58 @@ const APILinkButton = styledUI(Button)({
 
 
 export default function App() {
-  const { isLoading, error, data } = useQuery('repoData', () =>
+  const [getJoke, setGetJoke] = useState(false)
+  const [showPunch, setShowPunch] = useState(false)
+  const { isLoading, error, data, refetch } = useQuery('jokeData', () =>
     fetch('https://official-joke-api.appspot.com/random_joke').then(res =>
       res.json()
     )
   )
 
+  // why do my styles get jacked up without these???
   if (isLoading) return 'Loading...'
 
   if (error) return 'An error has occurred: ' + error.message
 console.log({data})
+
+const _onGetNewJoke = () => {
+  refetch
+  setShowPunch(false)
+  // window.location.reload(false);
+}
+const _onSetShowPunchLine = () => {
+  setShowPunch(!showPunch)
+}
   return (
 
 
     <Wrapper>
       <HeaderWrapper>
-          <GetJokeButton variant="contained">Get a new Random Joke</GetJokeButton>
+          <GetJokeButton variant="contained" onClick={_onGetNewJoke}>Get a new Random Joke</GetJokeButton>
           <APILinkButton href="https://github.com/15Dkatz/official_joke_api" color="primary"> View API Docs </APILinkButton>
       </HeaderWrapper>
      
-
-      <h1>{data.setup}</h1>
-      <HideJokeButton variant="contained">Hide Punchline</HideJokeButton>
-      <p>{data.punchline}</p>
-
-    </Wrapper>
+     {
+       isLoading &&
+       <h2> Loading...</h2>
+     }
+     {
+       error &&
+       <h2>{'An error has occurred: ' + error.message}</h2>
+     }
+     {
+       !isLoading && !error &&
+       <JokeWrapper>
+          <Setup>{data.setup}</Setup>
+          <PunchButtonWrapper>
+            <PunchButton variant="contained" onClick={_onSetShowPunchLine}>{showPunch ?  'Hide Punchline' : 'Show Punch Line' }</PunchButton>
+          </PunchButtonWrapper>
+            {
+              showPunch &&
+            <PunchLine>{data.punchline}</PunchLine>
+            }
+       </JokeWrapper>
+      }
+  </Wrapper>
   )
 }
